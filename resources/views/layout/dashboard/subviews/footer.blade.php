7aca1586bb04dcc5e83d7a4590894208b5d710b5
damian <dwoz137@gmail.com>
2023-04-15 15:10:21 +0200

    Dashboard footer initial layout

---
+++ resources/views/layout/dashboard/subviews/footer.blade.php
@@ -0,0 +1,6 @@
+<footer class="p-4 bg-green-50 text-gray-400 text-center text-lg">
+        <x-dashboard.sidebar.toggler />
+    <p>
+        Copyright by <a href="https://github.com/CatS0up" class="text-green-400 font-semibold">CatS0up</a>
+    </p>
+</footer>
