b0e3592c80c08c2d91759fb2aac241b13149e92b
damian <dwoz137@gmail.com>
2023-04-15 15:25:41 +0200

    Common layout subviews

---
+++ resources/views/layout/common/head.blade.php
@@ -0,0 +1,8 @@
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>{{ $appTitle }}</title>
+
+    @include('layout.common.css')
+    @yield('styles')
+</head>
