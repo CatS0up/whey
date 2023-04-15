1235f640c32f05cdb6c28ba97de484147b154ccb
damian <dwoz137@gmail.com>
2023-04-15 15:25:03 +0200

    Header dashboard subview

---
+++ resources/views/layout/dashboard/subviews/header.blade.php
@@ -0,0 +1,9 @@
+<header class="flex items-center justify-between p-4 bg-green-50/40">
+    <x-dashboard.sidebar.toggler/>
+    <section class="flex items-center ml-auto">
+        <button class="mr-12">
+            <span class="fa-solid fa-magnifying-glass text-2xl"></span>
+        </button>
+        <img src="https://samequizy.pl/wp-content/uploads/2017/02/filing_images_1a54c232400e.jpg" class="rounded-full max-w-[45px]">
+    </section>
+</header>
