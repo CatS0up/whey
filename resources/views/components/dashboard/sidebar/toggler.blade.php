af863af486cbdf39e68e4549d1baffe905ee098c
damian <dwoz137@gmail.com>
2023-04-29 17:03:26 +0200

    Sidebar toggler - remove unecessary empty object declaration in x-data

--- resources/views/components/dashboard/sidebar/toggler.blade.php
+++ resources/views/components/dashboard/sidebar/toggler.blade.php
@@ -1,3 +1,3 @@
-<button x-data="{}" @click="$dispatch('toggle-sidebar')" {{ $attributes->class(['block lg:hidden w-10 h-10 text-gray-900 outline-none transition-colors hover:bg-gray-100 focus:bg-gray-100 rounded-full']) }}>
+<button x-data @click="$dispatch('toggle-sidebar')" {{ $attributes->class(['block lg:hidden w-10 h-10 text-gray-900 outline-none transition-colors hover:bg-gray-100 focus:bg-gray-100 rounded-full']) }}>
     <span class="fa-sharp fa-solid fa-bars text-xl"></span>
 </button>
