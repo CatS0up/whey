d246b6880349c3ad173946cfc24b9057d6ce9a74
damian <dwoz137@gmail.com>
2023-04-29 18:03:57 +0200

    FileData data object

---
+++ app/DataObjects/FileData.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\DataObjects;
+
+use Spatie\LaravelData\Data;
+
+final class FileData extends Data
+{
+    public function __construct(
+        public readonly ?int $id,
+        public readonly string $name,
+        public readonly string $file_name,
+        public readonly string $mime_type,
+        public readonly ?string $path = null,
+        public readonly string $disk,
+        public readonly string $hash,
+        public readonly ?string $collection = null,
+        public readonly int $size,
+    ) {
+    }
+}
