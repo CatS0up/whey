eb2fde713d3fdedc84d864cd8263b498b8eec648
damian <dwoz137@gmail.com>
2023-04-29 18:02:38 +0200

    ExerciseData data object remove wireable interface and trait

--- app/DataObjects/ExerciseData.php
+++ app/DataObjects/ExerciseData.php
@@ -7,16 +7,12 @@ namespace App\DataObjects;
 use App\Enums\DifficultyLevel;
 use App\Enums\ExerciseType;
 use Carbon\Carbon;
-use Livewire\Wireable;
 use Spatie\LaravelData\Attributes\WithCast;
 use Spatie\LaravelData\Casts\EnumCast;
-use Spatie\LaravelData\Concerns\WireableData;
 use Spatie\LaravelData\Data;
 
-final class ExerciseData extends Data implements Wireable
+final class ExerciseData extends Data
 {
-    use WireableData;
-
     public function __construct(
         public readonly ?int $id,
         public readonly string $name,
