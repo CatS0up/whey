3f86ed03b9b5e3c290972bccf9c511fe3ff1d6b0
damian <dwoz137@gmail.com>
2023-05-25 12:57:14 +0200

    Media model
    With database table

---
+++ app/Models/Media.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\MorphTo;
+
+class Media extends Model
+{
+    use HasFactory;
+
+    /**
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+        'file_name',
+        'mime_type',
+        'path',
+        'disk',
+        'file_hash',
+        'collection',
+        'size',
+    ];
+
+    public function mediable(): MorphTo
+    {
+        return $this->morphTo();
+    }
+}
