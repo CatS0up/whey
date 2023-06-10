d9296d6964947faa62db0aef543e584c84647168
damian <dwoz137@gmail.com>
2023-06-10 15:51:20 +0200

    Move test disk preparation to TestCase base class

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -5,10 +5,25 @@ declare(strict_types=1);
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Support\Facades\Storage;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
 
     protected const TEST_DISK = 'test';
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+       $this->prepareTestingDisk();
+    }
+
+    protected function prepareTestingDisk(): void
+    {
+        Storage::fake(self::TEST_DISK);
+
+        config()->set('app.uploads.disk', self::TEST_DISK);
+    }
 }
