d6fa7a0fde894763a7fd39b0960c6baaa0ad98a7
damian <dwoz137@gmail.com>
2023-06-21 19:47:05 +0200

    Dimension value object fixes
    Const names and test namespace

--- app/ValueObjects/Media/Dimension.php
+++ app/ValueObjects/Media/Dimension.php
@@ -4,21 +4,23 @@ declare(strict_types=1);
 
 namespace App\ValueObjects\Media;
 
+use InvalidArgumentException;
+
 final class Dimension
 {
-    public const MAX_DIMENSION_PX_WIDTH = 1;
-    public const MAX_DIMENSION_PX_HEIGHT = 1;
+    public const MIN_DIMENSION_PX_WIDTH = 1;
+    public const MIN_DIMENSION_PX_HEIGHT = 1;
 
     public function __construct(
         public readonly int $width,
         public readonly int $height,
     ) {
-        if ($width < self::MAX_DIMENSION_PX_WIDTH) {
-            throw new \InvalidArgumentException('Width should be greater or equal to ' . self::MAX_DIMENSION_PX_WIDTH);
+        if ($width < self::MIN_DIMENSION_PX_WIDTH) {
+            throw new InvalidArgumentException('Width should be greater or equal to '.self::MIN_DIMENSION_PX_WIDTH);
         }
 
-        if ($height < self::MAX_DIMENSION_PX_HEIGHT) {
-            throw new \InvalidArgumentException('Height should be greater or equal to ' . self::MAX_DIMENSION_PX_HEIGHT);
+        if ($height < self::MIN_DIMENSION_PX_HEIGHT) {
+            throw new InvalidArgumentException('Height should be greater or equal to '.self::MIN_DIMENSION_PX_HEIGHT);
         }
     }
 }
--- tests/Unit/ValueObjects/Media/DimensionTest.php
+++ tests/Unit/ValueObjects/Media/DimensionTest.php
@@ -6,6 +6,7 @@ namespace Tests\Unit\ValueObjects\Media;
 
 use App\ValueObjects\Media\Dimension as MediaDimension;
 use PHPUnit\Framework\TestCase;
+use InvalidArgumentException;
 
 class DimensionTest extends TestCase
 {
@@ -14,10 +15,10 @@ class DimensionTest extends TestCase
      */
     public function it_should_throw_invalid_argument_exception_when_given_width_is_less_than_one(): void
     {
-       $this->expectException(\InvalidArgumentException::class);
-       $this->expectExceptionMessage('Width should be greater or equal to 1');
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage('Width should be greater or equal to 1');
 
-       new MediaDimension(-1, 100);
+        new MediaDimension(-1, 100);
     }
 
     /**
@@ -25,7 +26,7 @@ class DimensionTest extends TestCase
      */
     public function it_should_throw_invalid_argument_exception_when_given_height_is_less_than_one(): void
     {
-        $this->expectException(\InvalidArgumentException::class);
+        $this->expectException(InvalidArgumentException::class);
         $this->expectExceptionMessage('Height should be greater or equal to 1');
 
         new MediaDimension(100, -1);
