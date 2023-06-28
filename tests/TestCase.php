6913e79ae6a7d92acb34120d48a446b9a34e5a43
damian <dwoz137@gmail.com>
2023-06-28 19:44:19 +0200

    Refactor base TestCase class
    Add file and image helpers

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,27 +4,53 @@ declare(strict_types=1);
 
 namespace Tests;
 
+use App\DataObjects\FileData;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Http\UploadedFile;
 use Illuminate\Support\Facades\Storage;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
 
-    protected const TEST_DISK = 'test';
-    protected const DEFAULT_TESTING_FILENAME = 'thumbnail.jpg';
+    /** @var string */
+    protected const TEST_DISK = 'testing_disk';
+    /** @var string */
+    protected const TEST_IMAGE_NAME = 'testing_image.jpg';
+    /** @var int */
+    protected const DEFAULT_TEST_IMAGE_WIDTH = 100;
+    /** @var int */
+    protected const DEFAULT_TEST_IMAGE_HEIGHT = 150;
 
     protected function setUp(): void
     {
         parent::setUp();
 
-       $this->prepareTestingDisk();
+        $this->prepareTestDisk();
     }
 
-    protected function prepareTestingDisk(): void
+    protected function prepareTestDisk(): void
     {
         Storage::fake(self::TEST_DISK);
 
+        // Set testing drive for all uploading files
         config()->set('app.uploads.disk', self::TEST_DISK);
+    }
+
+    protected function createTestImage(
+        $name = self::TEST_IMAGE_NAME,
+        $width = self::DEFAULT_TEST_IMAGE_WIDTH,
+        $height = self::DEFAULT_TEST_IMAGE_HEIGHT,
+    ): UploadedFile {
+        return UploadedFile::fake()->image(
+            name: $name,
+            width: $width,
+            height: $height,
+        );
+    }
+
+    protected function readFullImagePath(FileData $fileData): string
+    {
+        return Storage::disk($fileData->disk)->path($fileData->path);
     }
 }
