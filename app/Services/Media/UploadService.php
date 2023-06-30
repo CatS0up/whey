7655c4a318fb620ab2207b508fd036b85e8e1043
damian <dwoz137@gmail.com>
2023-06-30 15:40:08 +0200

    Add thumbnailSmall() method to UploadService class

--- app/Services/Media/UploadService.php
+++ app/Services/Media/UploadService.php
@@ -4,20 +4,26 @@ declare(strict_types=1);
 
 namespace App\Services\Media;
 
+use App\Actions\Media\UploadSmallThumbnailAction;
 use App\Actions\Media\UploadThumbnailAction;
 use App\DataObjects\FileData;
 use Illuminate\Http\UploadedFile;
-use Livewire\TemporaryUploadedFile;
 
 class UploadService
 {
     public function __construct(
         private UploadThumbnailAction $uploadThumbnailAction,
+        private UploadSmallThumbnailAction $uploadSmallThumbnailAction,
     ) {
     }
 
-    public function thumbnail(UploadedFile|TemporaryUploadedFile $file): FileData
+    public function thumbnail(UploadedFile $file): FileData
     {
         return $this->uploadThumbnailAction->execute($file);
+    }
+
+    public function thumbnailSmall(UploadedFile $file): FileData
+    {
+        return $this->uploadSmallThumbnailAction->execute($file);
     }
 }
