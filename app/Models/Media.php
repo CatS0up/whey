2003681f4601e2f7846ef0c8f6c32e73e0be48f4
damian <dwoz137@gmail.com>
2023-07-04 21:56:17 +0200

    Add data class to Media model
    Add WithData trait and set  property

--- app/Models/Media.php
+++ app/Models/Media.php
@@ -4,13 +4,19 @@ declare(strict_types=1);
 
 namespace App\Models;
 
+use App\DataObjects\FileData;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\MorphTo;
+use Spatie\LaravelData\WithData;
 
 class Media extends Model
 {
     use HasFactory;
+    use WithData;
+
+    /** @var string */
+    protected $dataClass = FileData::class;
 
     /**
      * @var array<string>
