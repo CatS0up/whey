73f7b4cda03a44b8bb21ca508d4eb7b229eb000e
damian <dwoz137@gmail.com>
2023-07-15 12:38:08 +0200

    Add thumbnail traits and interfaces to Muscle model

--- app/Models/Muscle.php
+++ app/Models/Muscle.php
@@ -4,10 +4,19 @@ declare(strict_types=1);
 
 namespace App\Models;
 
+use App\DataObjects\MuscleData;
+use App\Models\Concerns\HasSmallThumbnail;
+use App\Models\Concerns\HasThumbnail;
+use App\Models\Contracts\SmallThumbnailInterface;
+use App\Models\Contracts\ThumbnailInterface;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\LaravelData\WithData;
 
-class Muscle extends Model
+class Muscle extends Model implements ThumbnailInterface, SmallThumbnailInterface
 {
     use HasFactory;
+    use HasSmallThumbnail;
+    use HasThumbnail;
+    use WithData;
 }
