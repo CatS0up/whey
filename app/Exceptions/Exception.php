fcca55ea465a86000e146e7501d8723c30d832ba
damian <dwoz137@gmail.com>
2023-07-17 16:45:17 +0200

    Make base Exception class constructor final
    Solving PHPStan error Unsafe usage of new static()

--- app/Exceptions/Exception.php
+++ app/Exceptions/Exception.php
@@ -4,8 +4,18 @@ declare(strict_types=1);
 
 namespace App\Exceptions;
 
+use Throwable;
+
 class Exception extends \Exception
 {
+    final public function __construct(
+        string $message = "",
+        int $code = 0,
+        Throwable|null $previous = null,
+    ) {
+        parent::__construct($message, $code, $previous);
+    }
+
     public static function because(string $message): self
     {
         return new static($message);
