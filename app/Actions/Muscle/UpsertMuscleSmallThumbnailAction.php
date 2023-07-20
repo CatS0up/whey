42881584f5fa722aab261529428b2cf7264f49b0
damian <dwoz137@gmail.com>
2023-07-20 14:29:55 +0200

    Fix UpsertMuscleSmallThumbnailAction
    Returned types, parameters name, namespace, variables names

--- app/Actions/Muscle/UpsertMuscleSmallThumbnailAction.php
+++ app/Actions/Muscle/UpsertMuscleSmallThumbnailAction.php
@@ -2,11 +2,12 @@
 
 declare(strict_types=1);
 
-namespace App\Action\Muscle;
+namespace App\Actions\Muscle;
 
 use App\Actions\Media\ImageResizeAction;
-use App\Actions\Media\UpsertThumbnailAction;
+use App\Actions\Media\UpsertSmallThumbnailAction;
 use App\DataObjects\FileData;
+use App\Models\Media;
 use App\Models\Muscle;
 use App\ValueObjects\Media\Dimension;
 use Illuminate\Http\UploadedFile;
@@ -14,27 +15,27 @@ use Illuminate\Http\UploadedFile;
 class UpsertMuscleSmallThumbnailAction
 {
     public function __construct(
-        private UpsertThumbnailAction $upsertThumbnailAction,
+        private UpsertSmallThumbnailAction $upsertThumbnailAction,
         private ImageResizeAction $imageResizeAction,
         private Muscle $muscle,
     ) {
     }
 
-    public function execute(int $id, UploadedFile $thumbnail): Muscle
+    public function execute(int $id, UploadedFile $file): Media
     {
         /** @var Muscle $target */
         $target = $this->muscle->query()->findOrFail($id);
 
-        $thumbnail = $this->upsertThumbnailAction->execute($target, $thumbnail);
+        $smallThumbnail = $this->upsertThumbnailAction->execute($target, $file);
 
         /** @var FileData $fileData */
-        $fileData = $thumbnail->getData();
+        $fileData = $smallThumbnail->getData();
         $this->imageResizeAction->execute(
             fileData: $fileData,
             dimension: new Dimension(Muscle::THUMBNAIL_WIDTH, Muscle::THUMBNAIL_HEIGHT),
             shouldAspectRatio: ImageResizeAction::SHOULD_ASPECT_RATIO,
         );
 
-        return $target;
+        return $smallThumbnail;
     }
 }
