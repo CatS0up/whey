e35bcfb30f0250c02169f3516ff792db15eb903a
damian <dwoz137@gmail.com>
2023-07-28 19:05:43 +0200

    Add HasMediableRelationship trait to Muscle model

--- app/Models/Muscle.php
+++ app/Models/Muscle.php
@@ -5,17 +5,17 @@ declare(strict_types=1);
 namespace App\Models;
 
 use App\DataObjects\MuscleData;
+use App\Models\Concerns\HasMediableRelationship;
 use App\Models\Concerns\HasSmallThumbnail;
 use App\Models\Concerns\HasThumbnail;
-use App\Models\Contracts\SmallThumbnailInterface;
-use App\Models\Contracts\ThumbnailInterface;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Spatie\LaravelData\WithData;
 
-class Muscle extends Model implements ThumbnailInterface, SmallThumbnailInterface
+class Muscle extends Model
 {
     use HasFactory;
+    use HasMediableRelationship;
     use HasSmallThumbnail;
     use HasThumbnail;
     use WithData;
