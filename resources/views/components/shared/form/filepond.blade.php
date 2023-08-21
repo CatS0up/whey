7fe20b33e59114db2343f34c2f4dc309bec03176
damian <dwoz137@gmail.com>
2023-08-21 20:00:54 +0200

    Filepond blade component fixes
    Add a wrapper to the component and enable the option to set initial files

--- resources/views/components/shared/form/filepond.blade.php
+++ resources/views/components/shared/form/filepond.blade.php
@@ -3,7 +3,7 @@
         @vite([
             'node_modules/filepond/dist/filepond.min.css',
             'node_modules/filepond-plugin-image-preview/dist/filepond-plugin-image-preview.min.css',
-            ])
+        ])
     @endonce
 @endpush
 
@@ -11,36 +11,41 @@
     'multiple' => false,
     'preview' => true,
     'previewMaxHeight' => 200,
+    'initialFiles' => [],
 ])
 
-<input
-    wire:ignore
-    x-data
-    x-init="$nextTick(() => {
-        FilePond.registerPlugin(FilePondPluginImagePreview);
-        const post = FilePond.create($refs.input);
-        post.setOptions({
-            allowMultiple: {{ $multiple ? 'true' : 'false' }},
-            allowImagePreview: {{ $preview ? 'true' : 'false' }},
-            imagePreviewMaxHeight: {{ $previewMaxHeight }},
-            server: {
-                process:(fieldName, file, metadata, load, error, progress, abort, transfer, options) => {
-                    @this.upload('{{ $attributes->whereStartsWith('wire:model')->first() }}', file, load, error, progress)
+<div wire:ignore>
+    <input x-data x-init="$nextTick(() => {
+            FilePond.registerPlugin(FilePondPluginImagePreview);
+
+            const post = FilePond.create(
+                $refs.input,
+                    {
+                        files: @js($initialFiles).map(path => ({ source: path })),
+                    },
+                );
+
+            post.setOptions({
+                allowMultiple: {{ $multiple ? 'true' : 'false' }},
+                allowImagePreview: {{ $preview ? 'true' : 'false' }},
+                imagePreviewMaxHeight: {{ $previewMaxHeight }},
+                server: {
+                    process:(fieldName, file, metadata, load, error, progress, abort, transfer, options) => {
+                        @this.upload('{{ $attributes->whereStartsWith('wire:model')->first() }}', file, load, error, progress)
+                    },
+                    revert: (filename, load) => {
+                        @this.removeUpload('{{ $attributes->whereStartsWith('wire:model')->first() }}', filename, load)
+                    },
                 },
-                revert: (filename, load) => {
-                    @this.removeUpload('{{ $attributes->whereStartsWith('wire:model')->first() }}', filename, load)
-                },
-            }
-        });
-    })"
-    x-ref="input"
-    type="file"  {{ $attributes }}/>
+            });
+        })" x-ref="input" type="file" {{ $attributes }} />
+</div>
 
 @push('scripts')
     @once
         @vite([
             'node_modules/filepond/dist/filepond.min.js',
             'node_modules/filepond-plugin-image-preview/dist/filepond-plugin-image-preview.min.js',
-            ])
+        ])
     @endonce
 @endpush
