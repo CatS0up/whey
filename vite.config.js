3719da3d356301ae2b6fee9a35126fd8ecd3593c
damian <dwoz137@gmail.com>
2023-09-17 17:22:45 +0200

    tippy.js installation and vite configuration

--- vite.config.js
+++ vite.config.js
@@ -9,11 +9,13 @@ export default defineConfig({
                 'resources/css/app.css',
                 'node_modules/filepond/dist/filepond.min.css',
                 'node_modules/filepond-plugin-image-preview/dist/filepond-plugin-image-preview.min.css',
+                'node_modules/tippy.js/dist/tippy.css',
 
                 // Scripts
                 'resources/js/app.js',
                 'node_modules/filepond/dist/filepond.min.js',
                 'node_modules/filepond-plugin-image-preview/dist/filepond-plugin-image-preview.min.js',
+                'node_modules/tippy.js/dist/tippy-bundle.umd.js',
             ],
             refresh: true,
         }),
