5320d1ef97caa9f8cfdfac265f0cf847c3e03d29
damian <dwoz137@gmail.com>
2023-09-20 22:45:48 +0200

    Fill additional phone fields in UserObserver

--- app/Observers/UserObserver.php
+++ app/Observers/UserObserver.php
@@ -8,5 +8,24 @@ use App\Models\User;
 
 class UserObserver
 {
+    public function creating(User $user): void
+    {
+        $this->upsertPhoneFields($user);
+    }
 
+    public function updating(User $user): void
+    {
+        $this->upsertPhoneFields($user);
+    }
+
+    private function upsertPhoneFields(User $user): void
+    {
+        if ($user->isDirty('phone') && $user->phone) {
+            $number = (string) $user->phone;
+            $user->phone_normalized = preg_replace('[^0-9]', '', $number);
+            $user->phone_national = preg_replace('[^0-9]', '', phone($number, $user->phone_country)->formatNational());
+            $user->phone_e164 = phone($number, $user->phone_country)->formatE164();
+            $user->phone_country = $user->phone->getCountry();
+        }
+    }
 }
