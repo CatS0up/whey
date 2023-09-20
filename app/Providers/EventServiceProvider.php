3de87c90d6611f2930a9492e7f66e40d82b6f565
damian <dwoz137@gmail.com>
2023-09-20 17:59:11 +0200

    Fix PHPDoc

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -27,7 +27,7 @@ class EventServiceProvider extends ServiceProvider
     /**
      * The model observers for your application.
      *
-     * @var array
+     * @var array<string, array<int, object|string>|object|string>
      */
     protected $observers = [
         Exercise::class => [ExerciseObserver::class],
