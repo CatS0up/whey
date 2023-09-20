da3e9652fcd867878d2f8426403533c36fb05a03
damian <dwoz137@gmail.com>
2023-09-20 17:41:58 +0200

    Restore Weight value object

---
+++ app/ValueObjects/Shared/Weight.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\ValueObjects\Shared;
+
+use App\Enums\WeightUnit;
+use InvalidArgumentException;
+use Stringable;
+
+final readonly class Weight implements Stringable
+{
+    private const MIN_VALUE = 0;
+
+    private function __construct(
+        public float $value,
+        public WeightUnit $unit,
+    ) {
+        if (self::MIN_VALUE > $value) {
+            throw new InvalidArgumentException('Value should be greater or equals to ' . self::MIN_VALUE);
+        }
+    }
+
+    /** {@inheritdoc} */
+    public function __toString()
+    {
+        $defaultConversion = $this->unit->defaultConversion();
+        $defaultConversionBaseUnit = $defaultConversion->baseUnit();
+        $valueInDefaultConversionUnit = $this->value * $this->unit->otherUnitValue($defaultConversion);
+
+        $base = intval($valueInDefaultConversionUnit);
+        $rest = ($valueInDefaultConversionUnit - $base) * $defaultConversion->otherUnitValue($defaultConversionBaseUnit);
+        $rest = round($rest, 3);
+
+        return "{$base}{$defaultConversion->label()} {$rest}{$defaultConversionBaseUnit->label()}";
+    }
+
+    public static function fromValueAndUnit(float $value, WeightUnit $unit): self
+    {
+        return new self($value, $unit);
+    }
+
+    public function toOtherUnit(WeightUnit $unit): self
+    {
+        $value = $this->value * $this->unit->otherUnitValue($unit);
+
+        return new self($value, $unit);
+    }
+}
