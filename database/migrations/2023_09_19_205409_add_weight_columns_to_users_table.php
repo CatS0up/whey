e2b815472a1a48b49c4f388517c2ef9196ebf2b3
damian <dwoz137@gmail.com>
2023-09-20 23:10:13 +0200

    Add weight and weight_unit columns to users table

---
+++ database/migrations/2023_09_19_205409_add_weight_columns_to_users_table.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class () extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table): void {
+            $table->after(
+                column: 'height_unit',
+                callback: function (Blueprint $table): void {
+                    $table->unsignedDouble('weight');
+                    $table->string('weight_unit', 5);
+                },
+            );
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table): void {
+            $table->removeColumn(['weight', 'weight_unit']);
+        });
+    }
+};
