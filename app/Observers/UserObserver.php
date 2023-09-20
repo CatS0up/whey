656be6c8db40ddca8dd90d488d094351bf68067a
damian <dwoz137@gmail.com>
2023-09-20 23:19:15 +0200

    Upsert bmi column in the UserObserver

--- app/Observers/UserObserver.php
+++ app/Observers/UserObserver.php
@@ -4,18 +4,26 @@ declare(strict_types=1);
 
 namespace App\Observers;
 
+use App\Actions\Shared\CalculateBmiAction;
 use App\Models\User;
 
 class UserObserver
 {
+    public function __construct(
+        private CalculateBmiAction $calculateBmiAction,
+    ) {
+    }
+
     public function creating(User $user): void
     {
         $this->upsertPhoneFields($user);
+        $this->upsertBmiField($user);
     }
 
     public function updating(User $user): void
     {
         $this->upsertPhoneFields($user);
+        $this->upsertBmiField($user);
     }
 
     private function upsertPhoneFields(User $user): void
@@ -26,6 +34,13 @@ class UserObserver
             $user->phone_national = preg_replace('[^0-9]', '', phone($number, $user->phone_country)->formatNational());
             $user->phone_e164 = phone($number, $user->phone_country)->formatE164();
             $user->phone_country = $user->phone->getCountry();
+        }
+    }
+
+    private function upsertBmiField(User $user): void
+    {
+        if (($user->isDirty('weight') && $user->weight) || ($user->isDirty('height') && $user->height)) {
+            $user->bmi = $this->calculateBmiAction->execute($user->weight, $user->height);
         }
     }
 }
