eeac4ca78d7d4f460397db77cf3e08eb9b04c629
damian <dwoz137@gmail.com>
2023-09-21 14:21:53 +0200

    Make the User model mediable

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,6 +9,8 @@ namespace App\Models;
 use App\Casts\Height;
 use App\Casts\Weight;
 use App\Enums\PhoneCountry;
+use App\Models\Concerns\HasSubdirectoryFilePath;
+use App\Models\Contracts\Mediable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
@@ -17,11 +19,17 @@ use Laravel\Sanctum\HasApiTokens;
 use Propaganistas\LaravelPhone\Casts\E164PhoneNumberCast;
 use Propaganistas\LaravelPhone\Casts\RawPhoneNumberCast;
 
-class User extends Authenticatable
+class User extends Authenticatable implements Mediable
 {
     use HasApiTokens;
     use HasFactory;
+    use HasSubdirectoryFilePath;
     use Notifiable;
+
+    /** @var int */
+    public const AVATAR_WIDTH = 50;
+    /** @var int */
+    public const AVATAR_HEIGHT = 50;
 
     /**
      * The attributes that are mass assignable.
@@ -62,6 +70,17 @@ class User extends Authenticatable
         'weight' => Weight::class,
         'height' => Height::class,
     ];
+
+    public function id(): int
+    {
+        return $this->id;
+    }
+
+    public function type(): string
+    {
+        return self::class;
+    }
+
 
     public function exercises(): HasMany
     {
