daa2ab0f2fcac1a1f9d96e8617538260cd53aefb
damian <dwoz137@gmail.com>
2023-09-22 01:13:50 +0200

    Add favicon

--- resources/views/layout/common/head.blade.php
+++ resources/views/layout/common/head.blade.php
@@ -2,6 +2,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>{{ $appTitle }}</title>
+    <link rel="icon" type="image/x-icon" href="{{ asset('favicon.ico') }}">
 
     @include('layout.common.css')
     @stack('styles')
