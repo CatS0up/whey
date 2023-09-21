374f0fa2b7c50e1e52024280ebb16b5103dcb08a
damian <dwoz137@gmail.com>
2023-09-22 01:03:53 +0200

    Unique indexes in the users table
    Phone columns and name

---
+++ database/migrations/2023_09_21_224930_add_unique_indexes_to_the_phone_columns_in_users_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->unique(['phone', 'phone_normalized', 'phone_national', 'phone_e164']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('user', function (Blueprint $table) {
+            $table->dropIndex(['phone', 'phone_normalized', 'phone_national', 'phone_e164']);
+        });
+    }
+};
