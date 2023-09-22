c5e9b50d150f7a1cb2877bbc7187e9cb38ea2861
damian <dwoz137@gmail.com>
2023-09-22 18:51:12 +0200

    Pint fixes

--- app/helpers.php
+++ app/helpers.php
@@ -2,8 +2,7 @@
 
 declare(strict_types=1);
 
-if (!function_exists('compare_float'))
-{
+if ( ! function_exists('compare_float')) {
     function compare_float(float $target, float $compareTo): bool
     {
         return abs($target - $compareTo) < PHP_FLOAT_EPSILON;
