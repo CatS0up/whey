f1e4797e7be68dc72fbe5a554184a28650f3192b
damian <dwoz137@gmail.com>
2023-09-22 22:29:06 +0200

    Pint fixes

--- database/migrations/2023_09_21_224930_add_unique_indexes_to_the_phone_columns_in_users_table.php
+++ database/migrations/2023_09_21_224930_add_unique_indexes_to_the_phone_columns_in_users_table.php
@@ -1,17 +1,18 @@
 <?php
 
+declare(strict_types=1);
+
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class () extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        Schema::table('users', function (Blueprint $table) {
+        Schema::table('users', function (Blueprint $table): void {
             $table->unique(['phone']);
             $table->unique(['phone_normalized']);
             $table->unique(['phone_national']);
@@ -24,7 +25,7 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::table('user', function (Blueprint $table) {
+        Schema::table('user', function (Blueprint $table): void {
             $table->dropIndex([
                 'users_phone_unique',
                 'users_phone_normalized_unique',
