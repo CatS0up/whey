19bc16f3ff584152bc994a12ce7e408f57ae192e
damian <dwoz137@gmail.com>
2023-09-22 21:46:32 +0200

    compare_float fixes
    Use delta value instead of PHP_FLOAT_EPSILON const

--- app/helpers.php
+++ app/helpers.php
@@ -5,6 +5,8 @@ declare(strict_types=1);
 if ( ! function_exists('compare_float')) {
     function compare_float(float $target, float $compareTo): bool
     {
-        return abs($target - $compareTo) < PHP_FLOAT_EPSILON;
+        $delta = 0.00001;
+
+        return abs($target - $compareTo) < $delta;
     }
 }
