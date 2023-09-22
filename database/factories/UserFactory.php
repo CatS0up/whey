17ebabdda09018e6415ab4cad49ba46e1e974882
damian <dwoz137@gmail.com>
2023-09-22 17:08:12 +0200

    Change default password in the user factory
    Because password is hasing in the User model mutator

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -34,7 +34,7 @@ class UserFactory extends Factory
             /** @see \App\Observers\UserObserver::upsertPhoneFields() */
             // 'phone_country' => 'PL',
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'password' => 'password',
             'remember_token' => Str::random(10),
             'weight' => Weight::fromValueAndUnit(
                 value: fake()->randomFloat(min: 1),
