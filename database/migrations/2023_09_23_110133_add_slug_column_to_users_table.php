0b220691edbcb040b0ea743f471463d17b20f504
damian <dwoz137@gmail.com>
2023-09-23 14:40:38 +0200

    Slug column after name column
    users table

--- database/migrations/2023_09_23_110133_add_slug_column_to_users_table.php
+++ database/migrations/2023_09_23_110133_add_slug_column_to_users_table.php
@@ -13,7 +13,7 @@ return new class () extends Migration {
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table): void {
-            $table->string('slug')->unique();
+            $table->string('slug')->unique()->after('name');
         });
     }
 
