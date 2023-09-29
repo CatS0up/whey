35e42d59f56321e36c33ece3e38d52e589d98953
damian <dwoz137@gmail.com>
2023-09-29 18:58:18 +0200

    Select shared form blade component modify
    New design

--- resources/views/components/shared/form/select/input.blade.php
+++ resources/views/components/shared/form/select/input.blade.php
@@ -1,3 +1,52 @@
-<select {{ $attributes->class(['px-2 py-1 border rounded bg-white focus:outline focus:outline-1 focus:outline-green-400']) }}>
-    {{ $slot }}
-</select>
+@props([
+'id',
+'label' => null,
+'invalid' => false,
+'required' => false,
+])
+
+<p class="relative">
+    <select id="{{ $id }}" placeholder=" " {{ $attributes->class([
+        'w-full
+        h-full
+        p-4
+        border
+        border-gray-400/75
+        bg-transparent
+        rounded
+        transition-colors
+        focus:outline
+        focus:outline-1
+        focus:outline-teal-500
+        focus:border-teal-500
+        peer',
+        'border-red-500 focus:outline-red-500 focus:border-red-500' => $invalid,
+        ]) }}>
+        {{ $slot }}
+    </select>
+
+    @if ($label)
+    <label for="{{ $id }}" @class([
+                'absolute
+                top-1/2
+                left-2
+                bg-white
+                text-gray-600
+                font-semibold
+                cursor-text
+                origin-left
+                whitespace-nowrap
+                transition
+                scale-75
+                -translate-y-[175%]
+                peer-focus:text-teal-500
+                peer-focus:bg-white
+                peer-focus:font-semibold
+                peer-focus:scale-75
+                ' ,
+                'text-red-600 peer-focus:text-red-500'=> $invalid,
+        ])>
+        {{ $label . ($required ? ' *' : '')}}
+    </label>
+    @endif
+</p>
