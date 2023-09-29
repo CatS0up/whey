fc6f5ca7c2edaad1d2fc82f0cb5c2c7335b1979d
damian <dwoz137@gmail.com>
2023-09-29 18:47:37 +0200

    EmailVerifyResendController controller class
    For resend email verify token when token was expired

---
+++ app/Http/Controllers/Auth/EmailVerifyResendController.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Controllers\Auth;
+
+use App\Actions\Auth\SendVerificationEmailAction;
+use App\Enums\SweetAlertToastType;
+use App\Http\Controllers\Controller;
+use App\Models\EmailVerify;
+use App\ViewModels\Auth\GetEmailVerifyResendViewModel;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\View\View;
+
+class EmailVerifyResendController extends Controller
+{
+    public function show(EmailVerify $token): View
+    {
+        return view(
+            view: 'auth.sections.email-verify-resend',
+            data: [
+                'model' => (new GetEmailVerifyResendViewModel($token))->toArray(),
+            ],
+        );
+    }
+
+    public function resend(EmailVerify $token, SendVerificationEmailAction $action): RedirectResponse
+    {
+        $action->execute($token->user->id);
+
+        return to_route('auth.login.show')
+            ->with(
+                key: SweetAlertToastType::Success->type(),
+                value: 'The verification link has been sent',
+            );
+    }
+}
