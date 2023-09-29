f3d7306f1153dcde409c69df7949384ed9412265
damian <dwoz137@gmail.com>
2023-09-29 18:54:25 +0200

    Button shared blade component modification
    Edit colors and default type

--- resources/views/components/shared/button.blade.php
+++ resources/views/components/shared/button.blade.php
@@ -1,10 +1,12 @@
 @props([
-    'type' => 'primary',
+    'variant' => 'primary',
+    'type' => 'button',
 ])
 
-<button {{ $attributes->class([
-    'block mt-4 px-4 py-2 transition-colors text-xs rounded-lg font-semibold',
-    'bg-green-500 text-green-50 hover:bg-green-600 focus:bg-green-600' => 'primary' === $type,
-    'bg-blue-500 text-blue-50 hover:bg-blue-600 focus:bg-blue-600' => 'secondary' === $type,
-    'bg-red-500 text-red-50 hover:bg-red-600 focus:bg-red-600' => 'danger' === $type,
+<button type="{{ $type }}" {{ $attributes->class([
+    'block min-w-fit p-3.5 text-sm transition-colors rounded font-semibold uppercase outline-none drop-shadow-md',
+    'bg-teal-500 text-teal-50 hover:bg-teal-600 focus:bg-teal-600 group-focus:bg-teal-600 disabled:bg-teal-600' => 'primary' === $variant,
+    'bg-emerald-500 text-emerald-50 hover:bg-emerald-600 focus:bg-emerald-600 group-focus:bg-emerald-600 disabled:bg-emerald-600' => 'secondary' === $variant,
+    'bg-red-500 text-red-50 hover:bg-red-600 focus:bg-red-600 group-focus:bg-red-600 disabled:bg-red-600' => 'danger' === $variant,
+    'bg-transparent text-gray-900 hover:bg-teal-50 focus:bg-teal-50 group-focus:bg-teal-50 border-solid border border-gray-900' => 'transparent' === $variant,
     ]) }} >{{ $slot }}</button>
