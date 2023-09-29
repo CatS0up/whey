0f00d11b8aa28258ac9d38320c2c83dd56c73d0c
damian <dwoz137@gmail.com>
2023-09-29 20:40:48 +0200

    Auth form blade components

---
+++ resources/views/components/auth/form/back-to-login-page-button.blade.php
@@ -0,0 +1,3 @@
+<a href="{{ route('auth.login.login') }}" {{ $attributes->class(['block outline-none group']) }}>
+    <x-shared.button variant="transparent" class="w-full">Wróć do logowania</x-shared.button>
+</a>
---
+++ resources/views/components/auth/form/title.blade.php
@@ -0,0 +1,1 @@
+<h1 class="mt-2 font-semibold text-2xl text-center">{{ $slot }}</h1>
