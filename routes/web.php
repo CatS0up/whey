a65bd6235de10c33db754386aa3e59322e11c554
damian <dwoz137@gmail.com>
2023-09-29 11:40:17 +0200

    Dashboard index endpoint with temporary view

--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::prefix('/')->as('shared.')->group(base_path('routes/shared.php'));
+Route::as('web.')->group(base_path('routes/web/web.php'));
---
+++ routes/web/web.php
@@ -0,0 +1,7 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Support\Facades\Route;
+
+Route::as('dashboard.')->group(base_path('routes/web/dashboard.php'));
