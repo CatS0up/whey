a5b9bdae08cfec8066f6606c07d24de027e28906
damian <dwoz137@gmail.com>
2023-10-01 17:18:14 +0200

    Add checked props to checkbox component

--- resources/views/components/shared/form/checkbox.blade.php
+++ resources/views/components/shared/form/checkbox.blade.php
@@ -4,6 +4,7 @@
     'name',
     'invalid' => false,
     'required' => false,
+    'checked' => false,
 ])
 <div {{ $attributes->class([
     'flex flex-row items-center',
@@ -12,6 +13,7 @@
         id="{{ $id }}"
         name="{{ $name }}"
         type="checkbox"
-        class="cursor-pointer">
+        class="cursor-pointer"
+        @checked($checked)>
     <x-shared.form.label for="{{ $id }}" @class(['text-red-400' => $invalid, 'ml-2'])>{{ $label . ($required ? ' *' : '')}}</x-shared.form.label>
 </div>
