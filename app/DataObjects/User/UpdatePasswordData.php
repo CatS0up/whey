1a1d039f0150f4a227912f3e64a43f401fc72f9c
damian <dwoz137@gmail.com>
2023-10-02 01:19:30 +0200

    UpdatePasswordData data class

---
+++ app/DataObjects/User/UpdatePasswordData.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\DataObjects\User;
+
+use App\Http\Requests\User\UpdatePasswordRequest;
+use Spatie\LaravelData\Data;
+
+final class UpdatePasswordData extends Data
+{
+    public function __construct(
+        public readonly int $userId,
+        public readonly string $password,
+    ) {
+    }
+
+    public static function fromFormRequest(UpdatePasswordRequest $request): self
+    {
+        return self::from([
+            'userId' => $request->user->id,
+            'password' => $request->password,
+        ]);
+    }
+}
