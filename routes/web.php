720d05d83194d74387b25dc4925dd253859d0a00
damian <dwoz137@gmail.com>
2023-10-01 12:31:20 +0200

    Fix middleware group name
    In the web routes group

--- routes/web.php
+++ routes/web.php
@@ -16,4 +16,4 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::as('shared.')->group(base_path('routes/shared.php'));
-Route::middleware('authentication')->as('web.')->group(base_path('routes/web/web.php'));
+Route::middleware('authenticate')->as('web.')->group(base_path('routes/web/web.php'));
