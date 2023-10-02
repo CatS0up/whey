d74691b13930d2ba20661a93dbad587ec740ea6d
damian <dwoz137@gmail.com>
2023-10-02 18:57:18 +0200

    User/PasswordValidator fixes
    Add missing key

--- app/Validators/User/PasswordValidator.php
+++ app/Validators/User/PasswordValidator.php
@@ -12,15 +12,17 @@ class PasswordValidator
     public function rules(): array
     {
         return [
-            'required',
-            'string',
-            'confirmed',
-            Password::min(User::MIN_PASSWORD_LENGTH)
-                ->letters()
-                ->mixedCase()
-                ->numbers()
-                ->symbols()
-                ->uncompromised(),
+            'password' => [
+                'required',
+                'string',
+                'confirmed',
+                Password::min(User::MIN_PASSWORD_LENGTH)
+                    ->letters()
+                    ->mixedCase()
+                    ->numbers()
+                    ->symbols()
+                    ->uncompromised(),
+            ],
         ];
     }
 }
