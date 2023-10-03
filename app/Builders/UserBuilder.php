90035cce5abce9c7486a24a455d6961ea37000dc
damian <dwoz137@gmail.com>
2023-10-03 22:57:49 +0200

    UserBuilder remove unused methods

--- app/Builders/UserBuilder.php
+++ app/Builders/UserBuilder.php
@@ -13,13 +13,4 @@ class UserBuilder extends Builder
     {
         return $this->whereEmail($email)->firstOrFail();
     }
-
-    public function wherePhoneNumber(string $phoneNumber): self
-    {
-        return $this->where(function (Builder $query) use ($phoneNumber): void {
-            $query->where('phone_normalized', 'LIKE', preg_replace('[^0-9]', '', $phoneNumber).'%')
-                ->orWhere('phone_national', 'LIKE', preg_replace('[^0-9]', '', $phoneNumber).'%')
-                ->orWhere('phone_e164', 'LIKE', preg_replace('[^+0-9]', '', $phoneNumber).'%');
-        });
-    }
 }
