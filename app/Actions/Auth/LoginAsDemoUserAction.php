9a27d7d0726a8743587aeabc62b1e9a285483475
damian <dwoz137@gmail.com>
2023-10-19 23:06:21 +0200

    LoginAsDemoUserAction action class
    For running a demo project

---
+++ app/Actions/Auth/LoginAsDemoUserAction.php
@@ -0,0 +1,54 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Actions\Auth;
+
+use App\Builders\UserBuilder;
+use App\Enums\Role;
+use App\Models\User;
+use Illuminate\Auth\AuthManager;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
+use RuntimeException;
+
+class LoginAsDemoUserAction
+{
+    public function __construct(
+        private User $user,
+        private AuthManager $auth,
+    ) {
+    }
+
+    public function execute(Role $role): User
+    {
+        if ( ! config('auth.demo_users_enable')) {
+            throw new RuntimeException('Demo users are disabled');
+        }
+
+        $user = $this->findOrFailUserByRole($role);
+
+        $this->auth->loginUsingId($user->id);
+
+        return $user;
+    }
+
+    /** @return Model&User */
+    private function findOrFailUserByRole(Role $role): User
+    {
+        return $this->user->query()
+            ->when(
+                value: Role::User === $role,
+                callback: fn (Builder $q): UserBuilder => $q->whereName('user'),
+            )
+            ->when(
+                value: Role::Trainer === $role,
+                callback: fn (Builder $q): UserBuilder => $q->whereName('trainer'),
+            )
+            ->when(
+                value: Role::Admin === $role,
+                callback: fn (Builder $q): UserBuilder => $q->whereName('admin'),
+            )
+            ->firstOrFail();
+    }
+}
---
+++ tests/Feature/Actions/Auth/LoginAsDemoUserActionTest.php
@@ -0,0 +1,80 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Feature\Actions\Auth;
+
+use App\Actions\Auth\LoginAsDemoUserAction;
+use App\Enums\Role;
+use Database\Seeders\DemoUserSeeder;
+use Database\Seeders\PermissionRoleSeeder;
+use Database\Seeders\PermissionSeeder;
+use Database\Seeders\RoleSeeder;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Config;
+use RuntimeException;
+use Tests\TestCase;
+
+class LoginAsDemoUserActionTest extends TestCase
+{
+    use RefreshDatabase;
+
+    private LoginAsDemoUserAction $actionUnderTest;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->actionUnderTest = app()->make(LoginAsDemoUserAction::class);
+    }
+
+    /** @test */
+    public function it_should_throw_a_RuntimeException_when_a_user_tries_to_log_in_as_one_from_the_demo_users_but_demo_users_are_disabled_in_the_config(): void
+    {
+        Config::set('auth.demo_users_enable', false);
+
+        $this->expectException(RuntimeException::class);
+        $this->expectExceptionMessage('Demo users are disabled');
+
+        $this->actionUnderTest->execute(Role::User);
+    }
+
+    /**
+     * @test
+     * @dataProvider usersProvider
+     * */
+    public function user_can_log_in_as_one_from_the_demo_users_when_users_are_enabled_in_the_config(Role $role, string $userName): void
+    {
+        Config::set('auth.demo_users_enable', true);
+        $this->seed([
+            RoleSeeder::class,
+            PermissionSeeder::class,
+            PermissionRoleSeeder::class,
+            DemoUserSeeder::class,
+        ]);
+
+        $authUser = $this->actionUnderTest->execute($role);
+
+        $this->assertEquals($userName, $authUser->name);
+        $this->assertAuthenticatedAs($authUser);
+    }
+
+    public static function usersProvider(): array
+    {
+        return [
+            // Grams - start
+            'log in as user' => [
+                Role::User,
+                'user',
+            ],
+            'log in as trainer' => [
+                Role::Trainer,
+                'trainer',
+            ],
+            'log in as admin' => [
+                Role::Admin,
+                'admin',
+            ],
+        ];
+    }
+}
