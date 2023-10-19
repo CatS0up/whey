f57eb0f7edcb099644a01b60d94070b26a019572
damian <dwoz137@gmail.com>
2023-10-19 21:49:05 +0200

    Assigning the default user role in the UserObserver
    User role

--- app/Observers/UserObserver.php
+++ app/Observers/UserObserver.php
@@ -5,11 +5,14 @@ declare(strict_types=1);
 namespace App\Observers;
 
 use App\Actions\Shared\CalculateBmiAction;
+use App\Enums\Role;
 use App\Models\User;
+use App\Services\User\RoleService;
 
 class UserObserver
 {
     public function __construct(
+        private RoleService $roleService,
         private CalculateBmiAction $calculateBmiAction,
     ) {
     }
@@ -19,10 +22,22 @@ class UserObserver
         $this->upsertBmiField($user);
     }
 
+    public function saved(User $user): void
+    {
+        $this->assingDefaultRole($user);
+    }
+
     private function upsertBmiField(User $user): void
     {
         if (($user->isDirty('weight') && $user->weight) || ($user->isDirty('height') && $user->height)) {
             $user->bmi = $this->calculateBmiAction->execute($user->weight, $user->height);
+        }
+    }
+
+    private function assingDefaultRole(User $user): void
+    {
+        if ($user->roles->isEmpty()) {
+            $this->roleService->giveRoles($user->id, [Role::User->value]);
         }
     }
 }
