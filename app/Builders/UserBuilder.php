810b51ee717287a5226a6a016e16132b0153d2cb
damian <dwoz137@gmail.com>
2023-10-19 21:42:34 +0200

    Add hasPermissionToBySlug() method to the UserBuilder

--- app/Builders/UserBuilder.php
+++ app/Builders/UserBuilder.php
@@ -10,11 +10,35 @@ use Illuminate\Database\Eloquent\Builder;
 /**
  * @template TModelClass of \App\Models\User
  * @extends Builder<TModelClass>
+ *
+ * @property User $model
  */
 class UserBuilder extends Builder
 {
     public function firstOrFailByEmail(string $email): User
     {
         return $this->whereEmail($email)->firstOrFail();
+    }
+
+    public function hasPermissionToBySlug(string $slug): bool
+    {
+        $roles = $this->model->roles;
+
+        if ($roles->contains('slug', $slug)) {
+            return true;
+        }
+
+        foreach ($roles as $role) {
+            if ($role->permissions->contains('slug', $slug)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public function hasRoleBySlug(string $slug): bool
+    {
+        return $this->model->roles->contains('slug', $slug);
     }
 }
