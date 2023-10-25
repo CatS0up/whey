d3f9cee101586fc582a7a8b4804969cd978c155b
damian <dwoz137@gmail.com>
2023-10-25 19:56:34 +0200

    DemoUserRoleSelector blade component fixes
    Namespace, classes and variants

---
+++ app/View/Components/Auth/DemoUserRoleSelector.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\View\Components\Auth;
+
+use App\Enums\Role;
+use Closure;
+use Illuminate\Contracts\View\View;
+use Illuminate\View\Component;
+
+class DemoUserRoleSelector extends Component
+{
+    public function __construct(
+        public Role $role,
+        public string $color,
+    ) {
+    }
+
+    public function render(): View|Closure|string
+    {
+        return view('components.auth.demo-user-role-selector');
+    }
+}
--- resources/views/components/auth/demo-user-role-selector.blade.php
+++ resources/views/components/auth/demo-user-role-selector.blade.php
@@ -10,7 +10,12 @@
 
     <div @click="submit" @keyup.enter="submit" tabindex="0" class="group outline-none cursor-pointer">
         <figure>
-            <div class="bg-{{ $color }}-200 transition-colors group-hover:bg-{{ $color }}-400 group-focus:bg-{{ $color }}-400 rounded-full">
+            <div @class([
+                'transition-colors rounded-full',
+                'bg-green-200 group-hover:bg-green-400 group-focus:bg-green-400' => 'green' === $color,
+                'bg-indigo-200 group-hover:bg-indigo-400 group-focus:bg-indigo-400' => 'indigo' === $color,
+                'bg-gray-200 group-hover:bg-gray-400 group-focus:bg-gray-400' => 'gray' === $color,
+            ])>
                 <img src="{{ asset('images/icons/'.$role->value.'.png') }}" alt="{{ $role->label() }}" class="p-4">
             </div>
             <figcaption class="mt-2 text-center font-semibold"> {{ $role->label() }} </figcaption>
