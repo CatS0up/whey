2613559e25c69511437bc4080c99a6f927add214
damian <dwoz137@gmail.com>
2023-10-28 19:04:53 +0200

    Shortening the notation of classes with padding

--- resources/views/components/auth/form/container.blade.php
+++ resources/views/components/auth/form/container.blade.php
@@ -1,5 +1,5 @@
 <section class="w-full lg:w-1/2 lg:pl-8">
-    <x-shared.card class="py-12 px-12 sm:px-24 md:px-32 lg:px-12 rounded-none lg:rounded-2xl">
+    <x-shared.card class="p-12 sm:px-24 md:px-32 lg:px-12 rounded-none lg:rounded-2xl">
         {{ $slot }}
     </x-shared.card>
 </section>
