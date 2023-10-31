ece6d30f087bbb3e80bfd4fbf04e9c5f12d4e079
damian <dwoz137@gmail.com>
2023-10-31 19:50:36 +0100

    Changing dashboard sidebar toggler styling

--- resources/views/components/dashboard/sidebar/toggler.blade.php
+++ resources/views/components/dashboard/sidebar/toggler.blade.php
@@ -1,3 +1,3 @@
-<button x-data @click="$dispatch('toggle-sidebar')" {{ $attributes->class(['block lg:hidden w-10 h-10 text-gray-900 outline-none transition-colors hover:bg-gray-100 focus:bg-gray-100 rounded-full']) }}>
+<x-shared.circle-button x-data @click="$dispatch('toggle-sidebar')" class="lg:hidden">
     <span class="fa-sharp fa-solid fa-bars text-xl"></span>
-</button>
+</x-shared.circle-button>
