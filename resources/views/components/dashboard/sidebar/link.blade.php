0d210ca2388c3b03f0661b64320e1e96a70551b9
damian <dwoz137@gmail.com>
2023-10-31 19:43:16 +0100

    Fix the formatting of the dashboard sidebar link in the blade component

--- resources/views/components/dashboard/sidebar/link.blade.php
+++ resources/views/components/dashboard/sidebar/link.blade.php
@@ -4,8 +4,17 @@
     'active' => false,
 ])
 <a href="{{ $target }}" {{ $attributes->class([
-    'block flex items-center p-2 rounded-lg transition-colors hover:bg-green-200/80 outline-none text-gray-500 hover:text-green-600 focus:bg-green-200/80 focus:text-green-600',
-    'bg-green-200/80 text-green-600 pointer-events-none cursor-default' => $active,
+    'block
+    flex
+    items-center
+    p-2
+    rounded-lg
+    transition-colors
+    outline-none
+    text-gray-500/80
+    hover:bg-gray-100/80
+    focus:bg-gray-100/80',
+    'bg-emerald-200/80 text-emerald-600 pointer-events-none cursor-default' => $active,
 ]) }} >
     <span class="{{ $icon }}"></span>
     {{ $slot }}
