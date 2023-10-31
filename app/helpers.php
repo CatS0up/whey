24c7590f42ac80993dfd887497091c8419a1e259
damian <dwoz137@gmail.com>
2023-10-31 17:40:46 +0100

    File helper functions
    file_full_path() and file_url() functions

--- app/helpers.php
+++ app/helpers.php
@@ -2,11 +2,27 @@
 
 declare(strict_types=1);
 
+use Illuminate\Support\Facades\Storage;
+
 if ( ! function_exists('compare_float')) {
     function compare_float(float $target, float $compareTo): bool
     {
         $delta = 0.00001;
 
         return abs($target - $compareTo) < $delta;
+    }
+
+    if ( ! function_exists('file_full_path')) {
+        function file_full_path(string $disk, string $path): string
+        {
+            return Storage::disk($disk)->path($path);
+        }
+    }
+
+    if ( ! function_exists('file_url')) {
+        function file_url(string $disk, string $path): string
+        {
+            return Storage::disk($disk)->url($path);
+        }
     }
 }
