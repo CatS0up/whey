b81202bd31fe6ce314e6e869c8cfbdf9b5cf8643
damian <dwoz137@gmail.com>
2023-10-31 17:53:11 +0100

    Add MuscleSeeder and MuscleThumbnailSeeder to the DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,6 +20,8 @@ class DatabaseSeeder extends Seeder
             PermissionSeeder::class,
             PermissionRoleSeeder::class,
             DemoUserSeeder::class,
+            MuscleSeeder::class,
+            MuscleThumbnailSeeder::class,
         ]);
     }
 }
