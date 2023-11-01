db84c7aea95a7859ed6d41a3324a6f5d451279c3
damian <dwoz137@gmail.com>
2023-11-01 16:03:48 +0100

    DemoUserSeeder fixes
    Shortening the assignment of roles to a user

--- database/seeders/DemoUserSeeder.php
+++ database/seeders/DemoUserSeeder.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace Database\Seeders;
 
 use App\Enums\Role as RoleType;
-use App\Models\Role;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Sequence;
 use Illuminate\Database\Seeder;
@@ -32,16 +31,16 @@ class DemoUserSeeder extends Seeder
         User::query()
             ->firstWhere('name', 'user')
             ->roles()
-            ->sync(Role::find(RoleType::User->id())->value('id'));
+            ->sync(RoleType::User->id());
 
         User::query()
             ->firstWhere('name', 'trainer')
             ->roles()
-            ->sync(Role::find(RoleType::Trainer->id())->value('id'));
+            ->sync(RoleType::Trainer->id());
 
         User::query()
             ->firstWhere('name', 'admin')
             ->roles()
-            ->sync(Role::find(RoleType::Admin->id())->value('id'));
+            ->sync(RoleType::Admin->id());
     }
 }
