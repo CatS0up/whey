7904a62bc2a8539de2f7fea59b1f8b8f0d9c2b3d
damian <dwoz137@gmail.com>
2023-11-01 17:19:49 +0100

    Registering the timestamp type from Doctrine
    Because changing the column type to timestamp was causing a dbal error

--- config/database.php
+++ config/database.php
@@ -2,6 +2,7 @@
 
 declare(strict_types=1);
 
+use Illuminate\Database\DBAL\TimestampType;
 use Illuminate\Support\Str;
 
 return [
@@ -150,4 +151,9 @@ return [
 
     ],
 
+    'dbal' => [
+        'types' => [
+            'timestamp' => TimestampType::class,
+        ],
+    ],
 ];
