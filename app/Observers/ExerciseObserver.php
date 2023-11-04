c6a3009a10596f15087bedb9a18f8dff54bac4e8
damian <dwoz137@gmail.com>
2023-11-04 18:17:30 +0100

    Assigning the initial status during the creation of an exercise in the ExerciseObserver

--- app/Observers/ExerciseObserver.php
+++ app/Observers/ExerciseObserver.php
@@ -4,12 +4,14 @@ declare(strict_types=1);
 
 namespace App\Observers;
 
+use App\Enums\ExerciseStatus;
 use App\Models\Exercise;
 
 class ExerciseObserver
 {
     public function saving(Exercise $exercise): void
     {
+        $this->assignInitialStatus($exercise);
         $this->prepareInstructionsRawValue($exercise);
     }
 
@@ -17,6 +19,19 @@ class ExerciseObserver
     {
         if ($exercise->isDirty('instructions_html')) {
             $exercise->instructions_raw = strip_tags($exercise->instructions_html);
+        }
+    }
+
+    private function assignInitialStatus(Exercise $exercise): void
+    {
+        if (empty($exercise->status)) {
+            if ($exercise->is_public) {
+                $exercise->status = ExerciseStatus::ForVerification;
+                return;
+            }
+
+            $exercise->status = ExerciseStatus::Private;
+            return;
         }
     }
 }
