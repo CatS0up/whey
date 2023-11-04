75b8854a256cf24d2eddf9d2d2091b3b738f9fc7
damian <dwoz137@gmail.com>
2023-11-04 11:29:23 +0100

    Dashboard sidebar modifications
    Colors

--- resources/views/components/dashboard/sidebar/link.blade.php
+++ resources/views/components/dashboard/sidebar/link.blade.php
@@ -3,19 +3,19 @@
     'target',
     'active' => false,
 ])
-<a href="{{ $target }}" {{ $attributes->class([
-    'block
-    flex
-    items-center
-    p-2
-    rounded-lg
-    transition-colors
-    outline-none
-    text-gray-500/80
-    hover:bg-gray-100/80
-    focus:bg-gray-100/80',
-    'bg-emerald-200/80 text-emerald-600 pointer-events-none cursor-default' => $active,
-]) }} >
+    <a href="{{ $target }}" {{ $attributes->class([
+        'block
+        flex
+        items-center
+        p-2
+        rounded-lg
+        transition-colors
+        outline-none
+        text-gray-500/80
+        hover:bg-gray-100/80
+        focus:bg-gray-100/80',
+        'bg-gray-100/80 pointer-events-none cursor-default' => $active,
+    ]) }} >
     <span class="{{ $icon }}"></span>
     {{ $slot }}
 </a>
