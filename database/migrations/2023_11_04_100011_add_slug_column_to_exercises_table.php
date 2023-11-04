c34cef21d6c702e63c679bfd4d9a11dace6d5066
damian <dwoz137@gmail.com>
2023-11-04 12:06:01 +0100

    Pint fixes

--- database/migrations/2023_11_04_100011_add_slug_column_to_exercises_table.php
+++ database/migrations/2023_11_04_100011_add_slug_column_to_exercises_table.php
@@ -1,17 +1,18 @@
 <?php
 
+declare(strict_types=1);
+
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class () extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        Schema::table('exercises', function (Blueprint $table) {
+        Schema::table('exercises', function (Blueprint $table): void {
             $table->string('slug')->unique()->after('name');
         });
     }
@@ -21,7 +22,7 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::table('exercises', function (Blueprint $table) {
+        Schema::table('exercises', function (Blueprint $table): void {
             $table->dropIndex('exercises_slug_unique');
             $table->dropColumn('slug');
         });
