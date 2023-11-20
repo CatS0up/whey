9a24eae9e6d5b084e784a922e7f10433d5eff2c2
damian <dwoz137@gmail.com>
2023-11-20 19:33:52 +0100

    Removal of all methods from ExerciseObserver except for prepareInstructionsFields
    This is because the methods responsible for status will be moved to the event, triggering the appropriate action

--- app/Observers/ExerciseObserver.php
+++ app/Observers/ExerciseObserver.php
@@ -4,34 +4,21 @@ declare(strict_types=1);
 
 namespace App\Observers;
 
-use App\Enums\ExerciseStatus;
 use App\Models\Exercise;
+use App\Support\HtmlPurifier;
 
 class ExerciseObserver
 {
     public function saving(Exercise $exercise): void
     {
-        $this->assignInitialStatus($exercise);
-        $this->prepareInstructionsRawValue($exercise);
+        $this->prepareInstructionsFields($exercise);
     }
 
-    private function prepareInstructionsRawValue(Exercise $exercise): void
+    private function prepareInstructionsFields(Exercise $exercise): void
     {
         if ($exercise->isDirty('instructions_html')) {
-            $exercise->instructions_raw = strip_tags($exercise->instructions_html);
-        }
-    }
-
-    private function assignInitialStatus(Exercise $exercise): void
-    {
-        if (empty($exercise->status)) {
-            if ($exercise->is_public) {
-                $exercise->status = ExerciseStatus::ForVerification;
-                return;
-            }
-
-            $exercise->status = ExerciseStatus::Private;
-            return;
+            $exercise->instructions_html = HtmlPurifier::purify($exercise->instructions_html);
+            $exercise->instructions_raw = HtmlPurifier::convertToRawText($exercise->instructions_html);
         }
     }
 }
