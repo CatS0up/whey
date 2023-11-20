a511b10d63f2329dec76c04bcee96f3fed3b02c1
damian <dwoz137@gmail.com>
2023-11-20 19:39:29 +0100

    Add the assignRoleToUser method to the Authorization Test Trait

--- tests/Concerns/Authorization.php
+++ tests/Concerns/Authorization.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Tests\Concerns;
 
 use App\Models\Permission;
+use App\Models\Role;
 use App\Models\User;
 use Database\Seeders\PermissionRoleSeeder;
 use Database\Seeders\PermissionSeeder;
@@ -35,5 +36,15 @@ trait Authorization
             ability: $permission->slug,
             callback: fn (): bool => $user->hasPermissionToBySlug($permission->slug),
         );
+    }
+
+    protected function assignRoleToUser(User $user, string $name): void
+    {
+        $permission = Role::query()
+            ->whereName($name)
+            ->orWhere('slug', $name)
+            ->firstOr(fn (): Role => Role::factory()->create(['name' => $name]));
+
+        $user->roles()->save($permission);
     }
 }
